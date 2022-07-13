5bf1784a1c68d0ad60b310588ea7f81b7d701bb9
“ddanadeb” <ddanadeb@gmail.com>
2022-07-14 01:21:41 +0200

    dodala sam novo

--- test/pages/page.js
+++ test/pages/page.js
@@ -2,12 +2,26 @@
 * main page object containing all methods, selectors and functionality
 * that is shared across all page objects
 */
-module.exports = class Page {
+module.exports = class Page  {
     /**
     * Opens a sub page of the page
     * @param path path of the sub page (e.g. /path/to/page.html)
     */
     open (path) {
-        return browser.url(`https://the-internet.herokuapp.com/${path}`)
+        return browser.url(`https://www.saucedemo.com/${path}`)
     }
-}
+    get logout () {
+        return $('input[type="logout_sidebar_link"]'); //ovde vrv treba href
+    }
+    async logout (username, password) {
+        await this.inputUsername.setValue(username);
+        await this.inputPassword.setValue(password);
+        await this.logout.click();
+    }
+   
+    }
+  
+       
+        
+    
+
